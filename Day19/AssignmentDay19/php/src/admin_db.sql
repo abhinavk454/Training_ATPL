1ffd4a27a76e478c5c7acb4bf8566e11fe7a7bab
Abhinav Kumar <abhinav.vikash2@gmail.com>
2022-07-05 14:48:30 +0530

    Session01 Done

--- Day19/AssignmentDay19/php/src/admin_db.sql
+++
@@ -1,8 +0,0 @@
-create table admin(
-    username varchar(20) not null,
-    password varchar(20) not null,
-    name varchar(20) not null,
-    primary key(username)
-)
-insert into admin
-values('admin', 'admin', 'admin');
--- Day19/AssignmentDay19/php/src/student.php
+++
@@ -1,2 +0,0 @@
-<?php
-include('./connect_db.php');
