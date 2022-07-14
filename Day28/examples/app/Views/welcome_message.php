46d7f09cb847fd11c8982703c1454e1919eb39ae
Abhinav Kumar <abhinav.vikash2@gmail.com>
2022-07-14 16:27:28 +0530

    Done for Day28

---
+++ Day28/examples/app/Views/welcome_message.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./Styles/welcome_message.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="icons"></div>
+        <div class="anchors">
+            <a href="#">Home</a>
+            <a href="#">Elements</a>
+            <a href="#">Pages</a>
+            <a href="#">News</a>
+            <a href="#">Campus</a>
+            <a href="#">Shop</a>
+            <a href="#">Contacts</a>
+        </div>
+    </div>
+    <div class="content">
+        <h1>Welcome to the <br>John Harrison University</h1>
+        <p>
+            Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing <br> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div class="buttons">
+            <button>Sign Up for Excursion</button>
+            <button>Learn More</button>
+        </div>
+    </div>
+</body>
+
+</html>
